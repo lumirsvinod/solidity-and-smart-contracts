69bcc30365156563a5ea53d47af4009aee724d16
Lumir S Vinod <107679514+lumirsvinod@users.noreply.github.com>
2026-01-12 12:22:57 +0530

    Implement basic voting system contract

---
+++ VotingSystem.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract VotingSystem{
+    struct Proposal{
+        uint id;
+        string name;
+        uint voteCount;
+    }
+
+    Proposal[] private voterNames;
+    mapping (address => bool) hasVote;
+
+    constructor(){
+        voterNames.push(Proposal(1, "Eleven", 0));
+        voterNames.push(Proposal(2, "Vecna", 0));
+    }
+
+    function listCandidates() public view returns (Proposal[] memory) {
+        return voterNames;
+    }
+
+    modifier Checking(){
+        address voter = msg.sender;
+        require(!hasVote[voter], "You already voted");
+        _;
+    }
+    function vote(uint _id) public Checking(){
+        voterNames[_id].voteCount++;
+        hasVote[msg.sender] = true;
+    }
+
+
+}
